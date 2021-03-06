f550e1a68d06e607bd89bfdc932812bb7629dded
Gonzalo <100429091@alumnos.uc3m.es>
2021-03-06 23:40:44 +0100

    Comentarios añadidos

--- p1_llamadas/myls.c
+++ p1_llamadas/myls.c
@@ -12,15 +12,12 @@ int main(int argc, char *argv[])
 
     char path[PATH_MAX];
 
-    if (argc < 1) {
-        printf("Not enough arguments\n");
-        return -1;
-    }
-
+    // Si tenemos mas de un argumento es que nos ha llegado una ruta
     if (argc > 1){
         directorio = opendir(argv[1]);
     }
 
+    // Si tenemos un solo argumento significa que no nos ha llegado una ruta y por lo tanto obtenemos la ruta actual
     if (argc == 1){
         if (getcwd(path, PATH_MAX) != NULL){
             directorio = opendir(path);
@@ -30,15 +27,18 @@ int main(int argc, char *argv[])
         }
     }
 
+    // Devolvemos un error si no hemos podido abrir el directorio
     if (directorio == NULL) {
         printf ("Cannot open directory");
         return -1;
     }
 
+    // Leemos las entradas del directorio y las imprimimos
     while ((lectura = readdir(directorio)) != NULL) {
         printf ("%s\n", lectura->d_name);
     }
 
+    //cerramos el directorio
     closedir (directorio);
     return 0;
 }
