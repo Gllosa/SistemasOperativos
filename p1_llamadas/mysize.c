2b445798516a8c980afeb1d947faa229278fcbde
Gonzalo <100429091@alumnos.uc3m.es>
2021-03-07 00:51:44 +0100

    mysize funcionando

--- p1_llamadas/mysize.c
+++ p1_llamadas/mysize.c
@@ -8,7 +8,32 @@
 
 int main(int argc, char *argv[])
 {
-	
+	int fd;
+
+	struct dirent *lectura;
+	DIR *directorio;
+
+	char path[PATH_MAX];
+
+	if (getcwd(path, PATH_MAX) == NULL){
+		perror("Unable to get current path");
+		return -1;
+	}
+
+    directorio = opendir(path);
+
+	while ((lectura = readdir(directorio)) != NULL) {
+        if (lectura -> d_type == DT_REG){
+			if ((fd = open(lectura -> d_name, O_RDONLY)) < 0){
+				perror("File could not be opened\n");
+				close(fd);
+			}
+			int posicion = lseek(fd, 0, SEEK_END);
+			printf("%s	%i\n", lectura -> d_name, posicion);
+			close(fd);
+		}
+	}
+	closedir(directorio);
 
 	return 0;
 }
